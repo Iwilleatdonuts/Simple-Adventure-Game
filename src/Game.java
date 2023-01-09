c627c7955abb82ee6f44cce8a5c3e6b1329a00ed
Tommy Tang <tjtang0310@gmail.com>
2023-01-09 16:46:52 -0500

    Begin to create the game

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Game {
     public static void main(String[] args) throws Exception {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to Timmy's Adventure!! Here is a list of what actions you can take.");
     }
 }
