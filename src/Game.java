4f1617d7432ec582c7d0653b060af774ab28d0b5
Tommy Tang <tjtang0310@gmail.com>
2023-01-10 23:20:30 -0500

    fix all bugs

--- src/Game.java
+++ src/Game.java
@@ -1,31 +1,25 @@
 import java.util.Scanner;
+
+import javax.xml.stream.util.StreamReaderDelegate;
 public class Game {
     static void action(int numOfRounds, String weapon, String armor, Scanner scanner) {
+        String[] smallEnemies;
         if(numOfRounds<=20){
-            String[] smallEnemies = {"a Slime(10 ATK, 50 HP)", "a Worm(15 ATK, 40 HP)", "a Birb(20 ATK, 30 HP)"};
+            smallEnemies = new String[]{"a Slime(10 ATK, 50 HP)", "a Worm(15 ATK, 40 HP)", "a Birb(20 ATK, 30 HP)"};
             int[] smallEnemiesATK = {10, 15, 20};
             int[] smallEnemiesHP = {50, 40, 30};
         } else if(numOfRounds>=21&&numOfRounds<=40){
-            String[] smallEnemies = {"a Slime(12 ATK, 55 HP)", "a Worm(18 ATK, 44 HP)", "a Birb(24 ATK, 33 HP)"};
+            smallEnemies = new String[]{"a Slime(12 ATK, 55 HP)", "a Worm(18 ATK, 44 HP)", "a Birb(24 ATK, 33 HP)"};
             int[] smallEnemiesATK = {12, 18, 24};
             int[] smallEnemiesHP = {55, 44, 33};
         } else if(numOfRounds>=41&&numOfRounds<=60){
-            String[] smallEnemies = {"a Slime(15 ATK, 60 HP)", "a Worm(21 ATK, 50 HP)", "a Birb(30 ATK, 40 HP)"};
+            smallEnemies = new String[]{"a Slime(15 ATK, 60 HP)", "a Worm(21 ATK, 50 HP)", "a Birb(30 ATK, 40 HP)"};
             int[] smallEnemiesATK = {15, 21, 30};
             int[] smallEnemiesHP = {60, 50, 40};
         } else {
-            String[] smallEnemies = {"a Slime(20 ATK, 100 HP)", "a Worm(30 ATK, 80 HP)", "a Birb(40 ATK, 100 HP)"};
+            smallEnemies = new String[]{"a Slime(20 ATK, 100 HP)", "a Worm(30 ATK, 80 HP)", "a Birb(40 ATK, 100 HP)"};
             int[] smallEnemiesATK = {20, 30, 40};
             int[] smallEnemiesHP = {100, 80, 60};
-        }
-        if(numOfRounds>=20){
-            String[] mediumEnemies = {"a Suicide Bomber(90 ATK, 15 HP)"};
-            int[] mediumEnemiesATK = {90};
-            int[] mediumEnemiesHP = {15};
-        } else if(numOfRounds>=21&&numOfRounds<=40){
-            String[] mediumEnemies = {"a Suicide Bomber(110 ATK, 20 HP)"};
-            int[] mediumEnemiesATK = {110};
-            int[] mediumEnemiesHP = {20};
         }
         int largeFuckingWormATK = 40;
         int largeFuckingWormHP = 100;
@@ -33,19 +27,22 @@ public class Game {
         int randomItem = (int)(Math.random()*items.length);
         int rng = (int)(Math.random()*100);
         int randomSmallEnemy = (int)(Math.random()*smallEnemies.length);
-        int randomMediumEnemy = (int)(Math.random()*mediumEnemies.length);
         if (numOfRounds<=3){
             System.out.println("You have found a mysterious box! You take a peek inside and find "+items[randomItem]);
         } else if (numOfRounds<=9&&numOfRounds>=4){
-            if (rng>=50){
+            if (rng>=70){
                 System.out.println("You have found a mysterious box! You take a peek inside and find "+items[randomItem]);
-            } else if (rng>=10&&rng<=49){
+            } else if (rng<=69){
                 System.out.println("Your path has been blocked by "+smallEnemies[randomSmallEnemy]);
-            } else {
-                System.out.println("Oh no! Your path has been blocked by "+mediumEnemies[randomMediumEnemy]);
             }
         } else if (numOfRounds==10){
             System.out.println("You are on wave 10! You path is blocked by your first boss, a Large Fucking Worm.");
+        } else if (numOfRounds>=11&&numOfRounds<=24){
+            if (rng>=80){
+                System.out.println("You have found a mysterious box! You take a peek inside and find "+items[randomItem]);
+            } else if (rng<=79){
+                System.out.println("Your path has been blocked by "+smallEnemies[randomSmallEnemy]);
+            }
         }
         System.out.println("You have now completed "+numOfRounds+" out of 100 rounds. Ready to continue?");
         scanner.nextLine();
