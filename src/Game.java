42321bbcf2a4165a4fc9d95e1b437b65f3b2ada2
Iwilleatdonuts <122302608+Iwilleatdonuts@users.noreply.github.com>
2023-01-28 10:53:09 -0500

    remade entire thing

--- src/Game.java
+++ src/Game.java
@@ -3,30 +3,37 @@ import java.util.Scanner;
 import javax.xml.stream.util.StreamReaderDelegate;
 public class Game {
     static void action(int numOfRounds, String weapon, String armor, Scanner scanner) {
-        String[] smallEnemies;
-        if(numOfRounds<=20){
-            smallEnemies = new String[]{"a Slime(10 ATK, 50 HP)", "a Worm(15 ATK, 40 HP)", "a Birb(20 ATK, 30 HP)"};
-            int[] smallEnemiesATK = {10, 15, 20};
-            int[] smallEnemiesHP = {50, 40, 30};
-        } else if(numOfRounds>=21&&numOfRounds<=40){
-            smallEnemies = new String[]{"a Slime(12 ATK, 55 HP)", "a Worm(18 ATK, 44 HP)", "a Birb(24 ATK, 33 HP)"};
-            int[] smallEnemiesATK = {12, 18, 24};
-            int[] smallEnemiesHP = {55, 44, 33};
-        } else if(numOfRounds>=41&&numOfRounds<=60){
-            smallEnemies = new String[]{"a Slime(15 ATK, 60 HP)", "a Worm(21 ATK, 50 HP)", "a Birb(30 ATK, 40 HP)"};
-            int[] smallEnemiesATK = {15, 21, 30};
-            int[] smallEnemiesHP = {60, 50, 40};
-        } else {
-            smallEnemies = new String[]{"a Slime(20 ATK, 100 HP)", "a Worm(30 ATK, 80 HP)", "a Birb(40 ATK, 100 HP)"};
-            int[] smallEnemiesATK = {20, 30, 40};
-            int[] smallEnemiesHP = {100, 80, 60};
-        }
-        int largeFuckingWormATK = 40;
-        int largeFuckingWormHP = 100;
+        String[] smallEnemies = {"a Slime(30 ATK, 140 HP)", "a Worm(50 ATK, 100 HP)", "a Birb(50 ATK, 80 HP)"};
+        int[] smallEnemiesATK = {30, 50, 50};
+        int[] smallEnemiesHP = {140, 100, 80};
+        String[] bosses = {"a Large Fucking Work(40 ATK, 100 HP)", "an Extremely Fat Squirrel(60 ATK, 140 HP)", "a Pedophilic Chef(75 ATK, 160 HP)", "The Donut Maker(100 ATK, 200 HP)"};
+        int[] bossesATK = {40, 60, 75, 100, 1000};
+        int[] bossesHP = {100, 140, 160, 200, 69};
         String[] items = {"a Donut(69 Heal)", "a Chicken(15 Heal)", "a Random Cabbage(5 Heal)", "a Stone Sword(20 ATK)", "an Iron Sword(30 ATK)", "a Titanium Sword(40 ATK)", "a Donut Smacker(69 ATK)", "a Leather Aromor(15 DEF)", "a Bronze Armor(35 DEF)", "a Golden Armor(50 DEF)", "a Donut Guard(69 DEF)"};
         int randomItem = (int)(Math.random()*items.length);
         int rng = (int)(Math.random()*100);
         int randomSmallEnemy = (int)(Math.random()*smallEnemies.length);
+            if(numOfRounds<=20){
+                smallEnemies = new String[]{"a Slime(10 ATK, 50 HP)", "a Worm(15 ATK, 40 HP)", "a Birb(20 ATK, 30 HP)"};
+                int[] smallEnemiesATK = {10, 15, 20};
+                int[] smallEnemiesHP = {50, 40, 30};
+            } else if(numOfRounds>=21&&numOfRounds<=40){
+                smallEnemies = new String[]{"a Slime(12 ATK, 55 HP)", "a Worm(18 ATK, 44 HP)", "a Birb(24 ATK, 33 HP)"};
+                int[] smallEnemiesATK = {12, 18, 24};
+                int[] smallEnemiesHP = {55, 44, 33};
+            } else if(numOfRounds>=41&&numOfRounds<=60){
+                smallEnemies = new String[]{"a Slime(15 ATK, 60 HP)", "a Worm(21 ATK, 50 HP)", "a Birb(30 ATK, 40 HP)"};
+                int[] smallEnemiesATK = {15, 21, 30};
+                int[] smallEnemiesHP = {60, 50, 40};
+            } else if (numOfRounds>=61&&numOfRounds<=94){
+                smallEnemies = new String[]{"a Slime(20 ATK, 100 HP)", "a Worm(30 ATK, 80 HP)", "a Birb(40 ATK, 60 HP)"};
+                int[] smallEnemiesATK = {20, 30, 40};
+                int[] smallEnemiesHP = {100, 80, 60};
+            } else if (numOfRounds>=95&&numOfRounds<=100){
+                smallEnemies = new String[]{"a Slime(30 ATK, 130 HP)", "a Worm(40 ATK, 100 HP)", "a Birb(50 ATK, 80 HP)"};
+                int[] smallEnemiesATK = {30, 40, 50};
+                int[] smallEnemiesHP = {130, 100, 80};
+            }
         if (numOfRounds<=3){
             System.out.println("You have found a mysterious box! You take a peek inside and find "+items[randomItem]);
         } else if (numOfRounds<=9&&numOfRounds>=4){
@@ -36,16 +43,47 @@ public class Game {
                 System.out.println("Your path has been blocked by "+smallEnemies[randomSmallEnemy]);
             }
         } else if (numOfRounds==10){
-            System.out.println("You are on wave 10! You path is blocked by your first boss, a Large Fucking Worm.");
+            System.out.println("You are on wave 10! Your path is blocked by your first boss, a Large Fucking Worm(40 ATK, 100 HP).");
         } else if (numOfRounds>=11&&numOfRounds<=24){
             if (rng>=80){
                 System.out.println("You have found a mysterious box! You take a peek inside and find "+items[randomItem]);
             } else if (rng<=79){
                 System.out.println("Your path has been blocked by "+smallEnemies[randomSmallEnemy]);
             }
+        } else if (numOfRounds==25){
+                System.out.println("You are on wave 25! Your path has been blocked by a boss, an Extremely Fat Squirrel(60 ATK, 140 HP).");
+        } else if (numOfRounds>=26&&numOfRounds<=49){
+            if (rng>=86){
+                System.out.println("You have found a mysterious box! You take a peek inside and find "+items[randomItem]);
+            } else if (rng<=85){
+                System.out.println("Your path has been blocked by "+smallEnemies[randomSmallEnemy]);
+            }
+        } else if (numOfRounds==50){
+                System.out.println("You are on wave 50! Your path has been blocked by a boss, a Pedophilic Chef(75 ATK, 160 HP");
+        } else if (numOfRounds>=51&&numOfRounds<=74){
+            if (rng>=90){
+                System.out.println("You have found a mysterious box! You take a peek inside and find "+items[randomItem]);
+            } else if (rng<=89){
+                System.out.println("Your path has been blocked by "+smallEnemies[randomSmallEnemy]);
+            }
+        } else if (numOfRounds == 75){
+                System.out.println("You are on wave 75! Here, you face your first Spooky Boss, The Donut Maker(100 ATK, 200 HP!");
+        } else if (numOfRounds>=76&&numOfRounds<=89){
+            if (rng>=95){
+                System.out.println("You have found a mysterious box! You take a peek inside and find "+items[randomItem]);
+            } else if (rng<=94){
+                System.out.println("Your path has been blocked by "+smallEnemies[randomSmallEnemy]);
+            }
+        } else if (numOfRounds==90){
+               System.out.println("You are on wave 90! Your path has been blocked by a Spooky Boss, a Large Suicide Bomber(1000 ATK, 69 HP");
+        } else if (numOfRounds>=91&&numOfRounds<=94){
+              System.out.println("Your path has been blocked by "+smallEnemies[randomSmallEnemy]);
+        } else if (numOfRounds>=95&&numOfRounds<=98){
+              System.out.println("Your path has been blocked by "+smallEnemies[randomSmallEnemy]);  
         }
         System.out.println("You have now completed "+numOfRounds+" out of 100 rounds. Ready to continue?");
-        scanner.nextLine();
+        String foo = scanner.nextLine();
+        
     }
     static void addSpace(String blank){
         for(int i = 0; i < 20; i++){
--- src/testRNG.java
+++ src/testRNG.java
@@ -1,8 +1,5 @@
 public class testRNG {
     public static void main(String[] args) {
-       for(int i =0;i<100;i++){
-        int rng = (int)Math.round((Math.random())*100);
-        System.out.println(rng);
-       }
+        
     }
 }
